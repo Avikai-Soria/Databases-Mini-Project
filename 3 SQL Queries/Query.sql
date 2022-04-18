888cd1c5d39e19392645beab365b166c7216fc5a
אביחי סוריה <soria@g.jct.ac.il>
2022-04-18 23:36:16 +0300

    3 Queries

--- 3 SQL Queries/Query.sql
+++ 3 SQL Queries/Query.sql
@@ -2,6 +2,13 @@ Select StartTime, EndTime, URL
 From Exhibition Natural Join ExhibitionAtDepartment
 Where Genre LIKE '_b%' and DepartmentId < 530 and DepartmentId > 330
 
-Select AVG(SalaryForCurator)
+Select AVG(SalaryForCurator), rank
 From Curator Natural Join CuratorAtExhibition Natural Join Exhibition
-Where Rank >= 7 and StartTime >= '1/1/2020' and EndTime <= '1/1/2022'
+Where Rank >= 7 and StartTime >= to_date('1/1/2020', 'dd/mm/yyyy') and EndTime <= to_date('1/1/2022', 'dd/mm/yyyy') and Genre LIKE '%pop%'
+Group By Rank
+
+Select Name, DatePurchased
+From Exhibit Natural Join ExhibitHistory
+Where Price > 700 and Status LIKE '%stolen%'
+Group By ExhibitId, Name, DatePurchased
+Having count(*) > 1
