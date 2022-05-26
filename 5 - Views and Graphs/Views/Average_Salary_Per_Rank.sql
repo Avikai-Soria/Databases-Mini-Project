9e4d0379d5b3df50851c588fa7393777c7eec790
Avikaielef <72797973+Avikai-Soria@users.noreply.github.com>
2022-05-26 17:09:55 +0300

    Delete 5 - Views and Graphs directory

--- 5 - Views and Graphs/Views/Average_Salary_Per_Rank.sql
+++
@@ -1,7 +0,0 @@
-create or replace view average_salary_per_rank
-(avgsalary, rank)
-as
-(Select AVG(SalaryForCurator), rank
-       From Curator Natural Join CuratorAtExhibition Natural Join Exhibition
-       Where Rank >= 7 and StartTime >= to_date('1/1/2020', 'dd/mm/yyyy') and EndTime <= to_date('1/1/2022', 'dd/mm/yyyy') and Genre LIKE '%pop%'
-       Group By Rank);
--- 5 - Views and Graphs/Views/Stolen_Items_History.sql
+++
@@ -1,6 +0,0 @@
-create or replace view stolen_items_history
-(itemname, itemprice)
-as
-Select Name, Price
-From Exhibit Natural Join ExhibitHistory
-Where Status LIKE '%stolen%';
