dbfb621aa962945789fd9f56fda179a0974de3c2
Avikaielef <72797973+Avikai-Soria@users.noreply.github.com>
2022-05-26 17:19:46 +0300

    Update Department_gen.py

--- 2 data insert/gen_sql/Department_gen.py
+++ 2 data insert/gen_sql/Department_gen.py
@@ -1,16 +1,23 @@
+import random
+
+from faker import Faker
+
+FAKE = Faker()
+
 
 def getDepartment(n: int):
     r = range(1, n + 1)
     DepartmentId = r
-
-    coloms_name = ("DepartmentId")
+    Contributor = list(map(lambda a: FAKE.last_name(), r))
+    Floor = list(map(lambda a: random.randint(1, 10), r))
+    coloms_name = ("Department_Id", "Contributor", "Floor")
     t_name = "Department"
 
     data = ""
     for i in r:
         data += (
-                f"insert into {t_name} (DepartmentId) values "
-                + f"({DepartmentId[i - 1]});\n"
+                f"insert into {t_name} ({', '.join(coloms_name)}) values "
+                + f"({DepartmentId[i - 1]}, '{Contributor[i-1]}', '{Floor[i-1]}');\n"
         )
 
     with open("../sql/gen_Department.sql", "w") as f:
